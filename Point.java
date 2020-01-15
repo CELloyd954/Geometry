b9db5ff317ee8926dcb0322e190caf72d827f705
Cayden Edward Lloyd <lloydce@s.dcsdk12.org>
2020-01-15 08:59:25 -0700

    created point

--- Point.java
+++ Point.java
@@ -1,3 +1,16 @@
 public class Point {
-	
+
+	public Point(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
 }
