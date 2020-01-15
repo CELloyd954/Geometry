ec672832724d51121b1fe02f0c8e45342d4ad54f
Cayden Edward Lloyd <lloydce@s.dcsdk12.org>
2020-01-15 09:18:02 -0700

    finished point

--- Point.java
+++ Point.java
@@ -1,5 +1,8 @@
 public class Point {
 
+	private double x;
+	private double y;
+
 	public Point(double x, double y) {
 		this.x = x;
 		this.y = y;
@@ -11,6 +14,12 @@ public class Point {
 
 	public double getY() {
 		return y;
+	}
+
+	public double distance(Point point2) {
+		double dx = Math.abs(point2.getX() - this.getX());
+		double dy = Math.abs(point2.getY() - this.getY());
+		return Math.sqrt((dx * dx) + (dy * dy));
 	}
 
 }
--- PointTester.java
+++ PointTester.java
@@ -1,17 +1,12 @@
 public class PointTester {
 	public static void main(String[] args) {
 		
-		double distance = 0;
-
 		Point point1 = new Point(3, 2);
 		Point point2 = new Point(4, 7);
 
-		distance = MATH.sqrt((MATH.abs(point2.getX() - point1.getX()) * (MATH.abs(point2.getX() - point1.getX()) + 
-					(MATH.abs(point2.getY() - point1.getY()) * (MATH.abs(point2.getY() - point1.getY()));
-
-		System.out.println("First point: (" + point1.getX() + ", " + point1.getY() + ")";
-		System.out.println("Second point: (" + point2.getX() + ", " + point2.getY() + ")";
-		System.out.println("Distance between points: " + distance);
+		System.out.println("First point: (" + point1.getX() + ", " + point1.getY() + ")");
+		System.out.println("Second point: (" + point2.getX() + ", " + point2.getY() + ")");
+		System.out.println("Distance between points: " + point1.distance(point2));
 
 	}
 }
